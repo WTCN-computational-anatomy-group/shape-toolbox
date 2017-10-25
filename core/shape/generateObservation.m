d4dc153117ce8c17b1faa3a066aca3ce21f6a42c
ybalbastre <yael.balbastre@gmail.com>
2017-10-25 16:01:42 +0100

    Adapt generateObservation for new naming convention

--- core/shape/generateObservation.m
+++ core/shape/generateObservation.m
@@ -1,6 +1,6 @@
 function f = generateObservation(model, opt)
 
-    z = mvnrnd(zeros(1,size(model.A,1)), inv(model.A+model.ww));
+    z = mvnrnd(zeros(1,size(model.regz,1)), inv(model.regz));
     v = reconstructVelocity('latent', z, 'subspace', model.w);
     clear z
     iphi = exponentiateVelocity(v, 'iphi', 'vs', opt.vs, 'prm', opt.prm);
