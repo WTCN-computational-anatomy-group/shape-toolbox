7153f28917d49ae4c2f094c9d85ab185c2e1ab78
ybalbastre <yael.balbastre@gmail.com>
2017-12-12 12:41:24 +0000

    Compute LogDet diffeo once and for all + write nifti header before saving the model

--- core/register/llPriorVelocity.m
+++ core/register/llPriorVelocity.m
@@ -1,18 +1,30 @@
 function ll = llPriorVelocity(v, varargin)
-% FORMAT ll = llPriorVelocity(v, ('fast'), ('vs',   vs), ('prm',  prm))
+%__________________________________________________________________________
+% Prior log-likelihood of the initial velocity 
+%    log p(V | L)
+%--------------------------------------------------------------------------
+% FORMAT ll = llPriorVelocity(v, ('fast'), ...)
 %
-% ** Required **
-% v    - Initial velocity
-% ** Optional **
-% fast - If 'fast', only compute terms that depend on v [default: false]
-% ** Keyword arguments **
-% vs   - Voxel size of the velocity lattice [1 1 1]
-% prm  - Parameters of the L operator (see spm_diffeo)
-%        [0.0001 0.001 0.2 0.05 0.2]
-% ** Output **
-% ll   - Log-likelihood of the initial velocity (prior term)
+% REQUIRED
+% --------
+% v      - Initial velocity
 %
-% Returns log p(V | L)
+% OPTIONAL
+% --------
+% fast   - If 'fast', only compute terms that depend on v [default: false]
+%
+% KEYWORD ARGUMENTS
+% -----------------
+% vs     - Voxel size of the velocity lattice [1 1 1]
+% prm    - Parameters of the L operator (see spm_diffeo)
+%          [0.0001 0.001 0.2 0.05 0.2]
+% logdet - Log determinant of the kernel (so that it is not computed
+%          several times)
+%
+% OUTPUT
+% ------
+% ll     - Log-likelihood of the initial velocity (prior term)
+%__________________________________________________________________________
     
 
     % --- Parse inputs
@@ -22,11 +34,16 @@ function ll = llPriorVelocity(v, varargin)
     p.addOptional('fast',    '', @(X) ischar(X) && strcmpi(X, 'fast'));
     p.addParameter('vs',     [1 1 1], @(X) isscalar(X) || checkarray(X));
     p.addParameter('prm',    [0.0001 0.001 0.2 0.05 0.2], @checkarray);
+    p.addParameter('logdet',  [], @isnumeric);
     p.addParameter('debug',  false, @isscalar);
     p.parse(v, varargin{:});
     fast = strcmpi(p.Results.fast, 'fast');
+    vs    = p.Results.vs;
+    prm   = p.Results.prm;
+    ld    = p.Results.logdet;
+    debug = p.Results.debug;
     
-    if p.Results.debug, fprintf('* llPriorVelocity\n'); end;
+    if debug, fprintf('* llPriorVelocity\n'); end;
     
     % --- Load in memory
     v = single(numeric(v));
@@ -38,19 +55,18 @@ function ll = llPriorVelocity(v, varargin)
     v(~isfinite(v)) = 0;
     
     % --- Compute log-likelihood
-    m = spm_diffeo('vel2mom', v, double([p.Results.vs p.Results.prm]));
+    m = spm_diffeo('vel2mom', v, double([vs prm]));
     ll = v(:)' * m(:);
     
     % --- Add constants w.r.t. R
     if ~fast
         ll = ll + count * log(2 * pi);
         
-        [~, ld] = spm_shoot_greens('kernel', double(lat), double([p.Results.vs p.Results.prm]));
-        ll = ll - ld(1);
-%         if obj.Debug
-%             warning(['LL: discard 0.5 * det(L). ' ...
-%                      'Don''t know how to compute it (with the kernel?).']);
-%         end;
+        if ~isempty(ld)
+            [~, ld] = spm_shoot_greens('kernel', double(lat), double([vs prm]));
+            ld = ld(1);
+        end
+        ll = ll - ld;
     end
     
     % --- Common factor
--- core/shape/llPriorSubspace.m
+++ core/shape/llPriorSubspace.m
@@ -1,5 +1,5 @@
-function ll = llPriorSubspace(W, WW, vs, prm)
-% FORMAT ll = lbSubspace(EW, EWW, vs, prm, (H, reg))
+function ll = llPriorSubspace(W, WW, vs, prm, ld)
+% FORMAT ll = lbSubspace(EW, EWW, vs, prm, (ld))
 % W   - MAP subspace W
 % WW  - MAP precision W'*L*W
 % vs  - Voxel size
@@ -11,9 +11,13 @@ function ll = llPriorSubspace(W, WW, vs, prm)
     dim = [size(W) 1 1 1];
     lat = dim(1:3);
     K   = dim(5);
+    
+    if nargin < 5
+        ld = proba('LogDetDiffeo', lat, vs, prm);
+    end
 
     ll = - prod(dim) * log(2*pi) ...
-         + K * proba('LogDetDiffeo', lat, vs, prm) ...
+         + K * ld ...
          - trace(numeric(WW));
      ll = ll * 0.5;
 
