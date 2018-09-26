163f72fd30fa31b6a77f1a8874289d7e889767b2
Yaël Balbastre <yael.balbastre@gmail.com>
2018-09-26 11:27:22 +0100

    Better visualisation

---
+++ core/register/animateDiffeo.m
@@ -0,0 +1,211 @@
+function animateDiffeo(iphi, varargin)
+% _________________________________________________________________________
+%
+% FORMAT animateDiffeo(iphi, (a), (iscat), ...)
+%
+% -------------------------------------------------------------------------
+%
+% DATA
+% ----
+% iphi   - Diffeomorphic flow (obtained from shootGeodesicInv)
+%
+% OPTIONAL
+% --------
+% a      - Image to deform [none]
+% iscat  - Image to deform is categorical and should be softmaxed [false]
+%
+% KEYWORD ARGUMENTS
+% -----------------
+% def        - Deformation video (output path) [do not generate]
+% grid       - Grid deformation video (output path) [do not generate]
+% image      - Image deformation video (output path) [do not generate]
+% slice      - Slice direction (1 = sagittal, 2 = coronal, [3] = axial)
+% x          - Slice location [sagittal: 1/3, axial/coronal: middle]
+% vs         - Lattice voxel size [1 1 1]
+% grid_space - Nb of voxels between lines in the grid [10 10 10]
+% loop       - Repeat frames to form a loop? [false]
+% duration   - Total duration of the video in sec [5]
+%
+% Note: Possible output extensions are .mp4, .avi, .gif 
+% _________________________________________________________________________
+
+    % Parse arguments
+    % ---------------
+    p = inputParser;
+    p.FunctionName = 'animateDiffeo';
+    p.addRequired('iphi',   @(X) isnumeric(X) && size(X,5) > 1);
+    p.addOptional('a',      []);
+    p.addOptional('iscat',  false, @(X) isscalar(X) && (isnumeric(X)||islogical(X)));
+    p.addParameter('def',   '',    @ischar);
+    p.addParameter('grid',  '',    @ischar);
+    p.addParameter('image', '',    @ischar);
+    p.addParameter('slice', 3,     @(X) isscalar(X) && isnumeric(X));
+    p.addParameter('x',     nan,   @(X) isscalar(X) && isnumeric(X));
+    p.addParameter('lat',   [],    @isnumeric);
+    p.addParameter('vs',  [1 1 1], @isnumeric);
+    p.addParameter('grid_space', [10 10 10], @isnumeric);
+    p.addParameter('loop',  false, @(X) isscalar(X) && (isnumeric(X)||islogical(X)));
+    p.addParameter('duration', 5,     @(X) isscalar(X) && isnumeric(X));
+    p.parse(iphi, varargin{:});
+    a     = p.Results.a;
+    iscat = p.Results.iscat;
+    def   = p.Results.def;
+    grid  = p.Results.grid;
+    image = p.Results.image;
+    slice = p.Results.slice;
+    x     = p.Results.x;
+    vs    = p.Results.vs;
+    grid_space = p.Results.grid_space;
+    loop       = p.Results.loop;
+    duration   = p.Results.duration;
+    
+    dim = [size(iphi) 1 1 1];
+    lat = dim(1:3);
+    if ~isempty(grid)
+        grid_image = zeros(lat, 'single');
+        grid_image(grid_space(1):grid_space(1):end,:,:) = 1;
+        grid_image(:,grid_space(2):grid_space(2):end,:) = 1;
+        grid_image(:,:,grid_space(3):grid_space(3):end) = 1;
+    end
+    
+    if ~isfinite(x)
+        switch slice
+            case {2,3}
+                x = ceil(dim(slice)/2);
+            case 1
+                x = ceil(dim(slice)/3);
+        end
+    end
+
+    N   = size(iphi, 5);
+    fig = figure;
+    if ~isempty(def)
+        frames_def(1:N) = struct('cdata',[],'colormap',[]);
+        id = spm_warps('identity', lat);
+        nrm = max(max(max(max(max(sum((iphi-id).^2, 4))))));
+    end
+    if ~isempty(grid)
+        frames_grid(1:N)  = struct('cdata',[],'colormap',[]);
+    end
+    if ~isempty(image)
+        frames_image(1:N) = struct('cdata',[],'colormap',[]);
+    end
+    fprintf('Build and record frames\n');
+    for i=1:N
+        fprintf('i = %3d', i);
+        
+        if ~isempty(def)
+            fprintf(' | Def');
+            figure(fig);
+            h = imshow_deformation(iphi(:,:,:,:,i)-id, vs, x, slice, nrm);
+            frames_def(i) = getframe(h.Parent);
+        end
+        
+        if ~isempty(grid)
+            fprintf(' | Grid');
+            g1 = warp(iphi(:,:,:,:,i), grid_image);
+            switch slice
+                case 1
+                    g1 = reshape(g1(x,:,:), [dim(2) dim(3)]);
+                case 2
+                    g1 = reshape(g1(:,x,:), [dim(1) dim(3)]);
+                case 3
+                    g1 = reshape(g1(:,:,x), [dim(1) dim(2)]);
+                otherwise
+                    error('not handled')
+            end
+            g1 = permute(g1, [2 1]);
+            figure(fig);
+            h = imagesc(g1);
+            colormap(gray)
+            axis off
+            switch slice
+                case 1
+                    daspect([1/vs(2) 1/vs(3) 1]);
+                case 2
+                    daspect([1/vs(1) 1/vs(3) 1]);
+                case 3
+                    daspect([1/vs(1) 1/vs(2) 1]);
+            end
+            frames_grid(i) = getframe(h.Parent);
+            clear g1
+        end
+            
+        if ~isempty(image)
+            fprintf(' | Image');
+            a1 = warp(iphi(:,:,:,:,i), a);
+            if iscat
+                a1 = reconstructProbaTemplate(a1);
+            end
+            figure(fig);
+            h = imshow_categorical(a1, vs, x, slice);
+            frames_image(i) = getframe(h.Parent);
+            clear a1
+        end
+        
+        fprintf('\n');
+        
+    end
+    close(fig);
+    
+    fprintf('Save frames\n');
+    
+    for j=1:3
+        switch j
+            case 1
+                fname  = def;
+                if isempty(fname)
+                    continue
+                end
+                frames = frames_def;
+            case 2
+                fname  = grid;
+                if isempty(fname)
+                    continue
+                end
+                frames = frames_grid;
+            case 3
+                fname  = image;
+                if isempty(fname)
+                    continue
+                end
+                frames = frames_image;
+        end
+        fprintf('%s\n', fname);
+    
+        if loop
+            iter = [1:N N-1:-1:2];
+        else
+            iter = 1:N;
+        end
+        framerate = numel(iter)/duration;
+        if endsWith(fname, 'avi')
+            vid = VideoWriter(fname);
+            vid.FrameRate = framerate;
+            open(vid);
+        elseif endsWith(fname, 'mp4')
+            vid = VideoWriter(fname, 'MPEG-4');
+            vid.FrameRate = framerate;
+            open(vid);
+        end
+        first_frame = true;
+        for i=iter
+            fprintf('i: %d\n', i);
+            if endsWith(fname, 'gif')
+                [imind, cm] = rgb2ind(frame2im(frames(i)), 256);
+                if first_frame
+                    imwrite(imind, cm, fname, 'gif', 'Loopcount', inf, 'DelayTime', 1/framerate); 
+                    first_frame = false;
+                else
+                    imwrite(imind, cm, fname, 'gif', 'WriteMode', 'append', 'DelayTime', 1/framerate); 
+                end
+            elseif endsWith(fname, 'avi') || endsWith(fname, 'mp4') 
+                writeVideo(vid, frames(i));
+            end
+        end
+        if endsWith(fname, 'avi') || endsWith(fname, 'mp4') 
+            close(vid);
+        end
+    end
+
+end
--- utility-functions/animatePG.m
+++ utility-functions/animatePG.m
@@ -1,27 +1,40 @@
 function frames = animatePG(varargin)
 % _________________________________________________________________________
 %
-% FORMAT (frames) = animatePG((dat), model, opt, (pg), (fname), ...)
+% FORMAT animatePG(dat, model, opt, (fname), ...) > Animate subject
+% FORMAT animatePG(model, opt, (fname), ...)      > Animate template
+% FORMAT animatePG(model, (fname), ...)           > Manually built model
 %
 % -------------------------------------------------------------------------
 %
 % DATA/MODEL (obtained from pgra_model/pgva_model)
 % ----------
-% dat    - Data of one subject
-% model  - Model
-% opt    - Option
+% dat    - Data of one subject      (from shape_model)
+% model  - Model                    (from shape_model or manually built)
+% opt    - Option                   (from shape_model)
 %
-% OPTIONAL
-% --------
-% pg     - Principal geodesic along which to shoot ([1], 2, ..., K)
+% Note: for manually built models, model should be a structure with fields:
+%   A     - Latent precision matrix (or its diagonal)
+%   w     - Principal subspace
+%   prm   - Regularisation parameters for Geodesic shooting
+%   a     - Template or Log-template
+%   (vs)  - Voxel size [1 1 1]
+%   (cat) - Is template categorical? [false]
+%
+% OPTIONAL ARGUMENTS
+% ------------------
 % fname  - Filename of the output video (.mp4, .avi, .gif) [do not write]
 %
 % KEYWORD ARGUMENTS
 % -----------------
-% sigma - Shooting range in terms of number of standard deviation [3]
-% slice  - Slice direction (1 = sagittal, 2 = coronal, [3] = axial)
-% x      - Slice location [sagittal: 1/3, axial/coronal: middle]
-% n      - Number of frames generated [21]
+% pg       - Principal geodesic along which to shoot ([1], 2, ..., K)
+% sigma    - Shooting range in terms of number of standard deviation [3]
+% slice    - Slice direction (1 = sagittal, 2 = coronal, [3] = axial)
+% x        - Slice location [sagittal: 1/3, axial/coronal: middle]
+% n        - Number of frames generated [21]
+% loop     - Repeat frames to form a loop? [false]
+% duration - Total duration of the video in sec [5]
+% middle   - Start from the middle frame (= no deformation) [false]
 %
 % OUTPUT
 % ------
@@ -29,67 +42,118 @@ function frames = animatePG(varargin)
 %          They can be given to the movie function.
 % _________________________________________________________________________
 
+    % ---------------------------------------------------------------------
     % Detect case
-    % -----------
     if nargin < 3 || ~isstruct(varargin{3})
         shoot_subject = false;
-        model = varargin{1};
-        opt   = varargin{2};
-        varargin = varargin(3:end);
+        model         = varargin{1};
+        if nargin < 2 || ~isstruct(varargin{2})
+            opt = struct;
+            varargin = varargin(2:end);
+        else
+            opt      = varargin{2};
+            varargin = varargin(3:end);
+        end
     else
         shoot_subject = true;
-        dat   = varargin{1};
-        model = varargin{2};
-        opt   = varargin{3};
-        varargin = varargin(4:end);
+        dat           = varargin{1};
+        model         = varargin{2};
+        opt           = varargin{3};
+        varargin      = varargin(4:end);
     end
-
+    
+    % ---------------------------------------------------------------------
     % Parse arguments
-    % ---------------
     p = inputParser;
     p.FunctionName = 'animatePG';
-    p.addOptional('pg',    1,    @(X) isscalar(X) && isnumeric(X));
-    p.addOptional('fname', '',   @ischar);
-    p.addParameter('sigma', 3,   @(X) isscalar(X) && isnumeric(X));
-    p.addParameter('slice', 3,   @(X) isscalar(X) && isnumeric(X));
-    p.addParameter('x',     nan, @(X) isscalar(X) && isnumeric(X));
-    p.addParameter('n',     21,  @(X) isscalar(X) && isnumeric(X));
+    p.addOptional('fname',     '',    @ischar);
+    p.addParameter('pg',       1,     @(X) isscalar(X) && isnumeric(X));
+    p.addParameter('sigma',    3,     @(X) isscalar(X) && isnumeric(X));
+    p.addParameter('slice',    3,     @(X) isscalar(X) && isnumeric(X));
+    p.addParameter('x',        nan,   @(X) isscalar(X) && isnumeric(X));
+    p.addParameter('n',        21,    @(X) isscalar(X) && isnumeric(X));
+    p.addParameter('loop',     false, @(X) isscalar(X) && (isnumeric(X)||islogical(X)));
+    p.addParameter('duration', 5,     @(X) isscalar(X) && isnumeric(X));
+    p.addParameter('middle',   false, @(X) isscalar(X) && (isnumeric(X)||islogical(X)));
     p.parse(varargin{:});
-    pg    = p.Results.pg;
-    fname = p.Results.fname;
-    sigma = p.Results.sigma;
-    slice = p.Results.slice;
-    x     = p.Results.x;
-    n     = p.Results.n;
+    pg       = p.Results.pg;
+    fname    = p.Results.fname;
+    sigma    = p.Results.sigma;
+    slice    = p.Results.slice;
+    x        = p.Results.x;
+    n        = p.Results.n;
+    loop     = p.Results.loop;
+    duration = p.Results.duration;
+    middle   = p.Results.middle;
     
-    A   = model.z.A;
-    w   = model.pg.w;
-    vs  = opt.tpl.vs;
-    prm = opt.pg.prm;
-    iscat = opt.tpl.cat && checkarray(model.tpl.a);
-    if iscat && checkarray(model.tpl.a)
-        mu = model.tpl.a;
-    else
-        mu = model.tpl.mu;
+    
+    % ---------------------------------------------------------------------
+    % Read model
+    try
+        % --- Model obtained from shape_model
+        A   = model.z.A;
+        w   = model.pg.w;
+        vs  = opt.tpl.vs;
+        prm = opt.pg.prm;
+        iscat = opt.tpl.cat && checkarray(model.tpl.a);
+        if iscat && checkarray(model.tpl.a)
+            mu = model.tpl.a;
+        else
+            mu = model.tpl.mu;
+        end
+    catch
+        try
+            % --- "Manual" model
+            A = model.A;
+            if size(A,1) == 1 || size(A,2) == 1
+                A = diag(A);
+            end
+            w = model.w;
+            if isfield(model, 'vs')
+                vs = model.vs;
+            else
+                vs = [1 1 1];
+            end
+            prm = model.prm;
+            if isfield(model, 'cat')
+                iscat = model.cat;
+            else
+                iscat = false;
+            end
+            mu = model.a;
+        catch
+            error('The model is not organised as expected');
+        end
     end
     
+    % ---------------------------------------------------------------------
+    % Discretize z space
     cov  = inv(A);
     sd   = sqrt(cov(pg,pg));
     allz = linspace(-sigma*sd, sigma*sd, n);
     
+    % ---------------------------------------------------------------------
+    % Prepare output
     loops = numel(allz);
     frames(loops) = struct('cdata',[],'colormap',[]);
     i = 1;
     fprintf('Build and record frames\n');
     fig = figure;
+    
+    % ---------------------------------------------------------------------
+    % If subject: start from its z coordinates
     if shoot_subject
-        true_z   = dat.z.z;
-        mean = reconstructVelocity('latent', true_z, 'subspace', w);
-        r    = numeric(dat.v.v) - mean;
+        true_z = dat.z.z;
+        mean   = reconstructVelocity('latent', true_z, 'subspace', w);
+        r      = numeric(dat.v.v) - mean;
         clear mean
     end
+    
+    % ---------------------------------------------------------------------
+    % Loop: build transformation and save frame
     for z=allz
         fprintf('z: %f\n', z);
+        % --- Compute velocity
         if shoot_subject
             z1 = true_z;
             z1(pg) = z;
@@ -97,45 +161,47 @@ function frames = animatePG(varargin)
         else
             v = w(:,:,:,:,pg) * z;
         end
+        % --- Warp template
         iphi = exponentiateVelocity(v, 'iphi', 'vs', vs, 'prm', prm);
         mu1 = warp(iphi, mu);
         if iscat
             mu1 = reconstructProbaTemplate(mu1);
         end
-        mu1 = colorimage(mu1, x, slice);
-        image(mu1);
-        switch slice
-            case 1
-                daspect([1/vs(2) 1/vs(3) 1]);
-            case 2
-                daspect([1/vs(1) 1/vs(3) 1]);
-            case 3
-                daspect([1/vs(1) 1/vs(2) 1]);
-        end
-        axis off
-        drawnow
-        frames(i) = getframe(gcf);
+        % --- Save frame
+        h = imshow_categorical(mu1, vs, x, slice);
+        frames(i) = getframe(h.Parent);
         i = i + 1;
     end
     close(fig);
     
+    % ---------------------------------------------------------------------
+    % Create final video
     fprintf('Save frames\n');
     
-    if endsWith(fname, 'avi')
-        vid = VideoWriter(fname);
-        open(vid);
-    elseif endsWith(fname, 'mp4')
-        vid = VideoWriter(fname, 'MPEG-4');
+    if      loop &&  middle, iter = [ceil(n/2):n n:-1:1 1:ceil(n/2)];
+    elseif  loop && ~middle, iter = [1:n n:-1:1];
+    elseif ~loop &&  middle, iter = [ceil(n/2):n n:-1:ceil(n/2)];
+    elseif ~loop && ~middle, iter = 1:n;
+    end
+    framerate = numel(iter)/duration;
+    if endsWith(fname, 'mp4'), vidopt = {'MPEG-4'};
+    else,                      vidopt = {};
+    end
+    if endsWith(fname, 'avi') || endsWith(fname, 'mp4')
+        vid = VideoWriter(fname, vidopt{:});
+        vid.FrameRate = framerate;
         open(vid);
     end
-    for i=[ceil(n/2):n n:-1:1 1:ceil(n/2)]
+    first_frame = true;
+    for i=iter
         fprintf('z: %f\n', allz(i));
         if endsWith(fname, 'gif')
             [imind, cm] = rgb2ind(frame2im(frames(i)), 256);
-            if i == 1
-                imwrite(imind, cm, fname, 'gif', 'Loopcount', inf); 
+            if first_frame
+                imwrite(imind, cm, fname, 'gif', 'Loopcount', inf, 'DelayTime', 1/framerate); 
+                first_frame = false;
             else
-                imwrite(imind, cm, fname, 'gif', 'WriteMode', 'append'); 
+                imwrite(imind, cm, fname, 'gif', 'WriteMode', 'append', 'DelayTime', 1/framerate); 
             end
         elseif endsWith(fname, 'avi') || endsWith(fname, 'mp4') 
             writeVideo(vid, frames(i));
@@ -145,29 +211,4 @@ function frames = animatePG(varargin)
         close(vid);
     end
 
-end
-
-function mu = colorimage(mu, z, slice)
-    if nargin < 3
-        slice = 3;
-    end
-    dim = [size(mu) 1 1];
-    if nargin < 2 || ~isfinite(z)
-        switch slice
-            case {2,3}
-                z = ceil(dim(slice)/2);
-            case 1
-                z = ceil(dim(slice)/3);
-        end
-    end
-    switch slice
-        case 1
-            mu = catToColor(reshape(mu(z,:,:,:), [dim(2) dim(3) dim(4)]));
-        case 2
-            mu = catToColor(reshape(mu(:,z,:,:), [dim(1) dim(3) dim(4)]));
-        case 3
-            mu = catToColor(reshape(mu(:,:,z,:), [dim(1) dim(2) dim(4)]));
-        otherwise
-            error('not handled')
-    end
 end
